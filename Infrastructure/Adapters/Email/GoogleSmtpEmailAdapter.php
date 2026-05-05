29f7ebbc851a9f8e85f2d1eb1fe9ea9af563a48a
Enmanuel-caraballo <enmanuelcm03@gmail.com>
2026-05-04 21:47:17 -0500

    SMTP MailTrap configuration

--- Infrastructure/Adapters/Email/GoogleSmtpEmailAdapter.php
+++ Infrastructure/Adapters/Email/GoogleSmtpEmailAdapter.php
@@ -15,12 +15,12 @@ final class GoogleSmtpEmailAdapter implements SentVerificationEmailPort
         $mail = new PHPMailer(true);
         try {
             $mail->isSMTP();
-            $mail->Host = 'smtp.gmail.com';
+            $mail->Host = getenv('SMTP_HOST') ?: 'sandbox.smtp.mailtrap.io';
             $mail->SMTPAuth = true;
             $mail->Username = getenv('SMTP_USER') ?: '';
             $mail->Password = getenv('SMTP_PASS') ?: '';
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-            $mail->Port = 465;
+            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+            $mail->Port = getenv('SMTP_PORT') ?: 2525;
             $mail->CharSet = 'UTF-8';
 
             $mail->setFrom('enmanuelcm03@gmail.com', 'Users system');
